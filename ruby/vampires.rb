f6a557a48f2b1667f5d66541aee420d4ae8f951b
Tommy <thomaskclark@gmail.com>
2016-12-06 21:29:54 -0800

    add plot twist and update code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,46 +11,48 @@ emp_age = gets.chomp.to_i
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order you some? [Y/N]"
-wants_garlic_bread = gets.chomp.upcase
-until (wants_garlic_bread == "Y" or wants_garlic_bread == "N")
-    puts "Please enter Y or N." 
-    wants_garlic_bread = gets.chomp.upcase
+puts "Our company cafeteria serves garlic bread. Should we order you some? (Y/N)"
+garlic_bread = gets.chomp.upcase
+until (garlic_bread == "Y" or garlic_bread == "N")
+	puts "Please enter 'Y' or 'N'." 
+	garlic_bread = gets.chomp.upcase
 end
 
-if wants_garlic_bread == "Y"
-	wants_garlic_bread = true
-else wants_garlic_bread = false
+if garlic_bread == "Y"
+	garlic_bread = true
+else garlic_bread = false
 end
 
-puts "Would you like to enroll in the company's health insurance? [Y/N]"
-wants_health_ins = gets.chomp.upcase
-until (wants_health_ins == "Y" or wants_health_ins == "N")
-    puts "Please enter Y or N." 
-    wants_health_ins = gets.chomp.upcase
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+health_insurance = gets.chomp.upcase
+until (health_insurance == "Y" or health_insurance == "N")
+	puts "Please enter 'Y' or 'N'." 
+	health_insurance = gets.chomp.upcase
 end
 
-if wants_health_ins == "Y"
-	wants_health_ins = true
-else wants_health_ins = false
+if health_insurance == "Y"
+	health_insurance = true
+else health_insurance = false
 end
 
-puts "Please list ALL allergies, one at a time. Type 'done' when finished."
-  done = false
-  until done
-    allergy = gets.chomp.downcase
-    break if allergy == "done"
-    if allergy == "sunshine"
-      done = true
-    end
-  end
+puts "Please list ALL allergies, one at a time. Enter 'done' once you are finished."
+	done = false
+	until done
+		allergy = gets.chomp.downcase
+		break if allergy == "done"
+		if allergy == "sunshine"
+			done = true
+		end
+	end
 
-result = nil
-result = "Probably not a vampire." if birth_year == Time.now.year - emp_age && (wants_garlic_bread || wants_health_ins)
-result = "Probably a vampire." if birth_year != Time.now.year - emp_age && (!wants_garlic_bread || !wants_health_ins) || allergy == "sunshine"
-result = "Almost certainly a vampire." if birth_year != Time.now.year - emp_age && (!wants_garlic_bread || !wants_health_ins)
-result = "Definitely a vampire." if emp_name.downcase == "drake cula" || emp_name.downcase == "tu fang"
-result = "Results inconclusive." if result == nil
+verdict = nil
+verdict = "Probably not a vampire." if birth_year == Time.now.year - emp_age && (garlic_bread || health_insurance)
+verdict = "Probably a vampire." if birth_year != Time.now.year - emp_age && (!garlic_bread || !health_insurance) || allergy == "sunshine"
+verdict = "Almost certainly a vampire." if birth_year != Time.now.year - emp_age && (!garlic_bread || !health_insurance)
+verdict = "Definitely a vampire." if emp_name.downcase == "drake cula" || emp_name.downcase == "tu fang"
+verdict = "Results inconclusive." if verdict == nil
 
-puts result
+puts verdict
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
