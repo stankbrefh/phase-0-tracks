bbf31bba1cb49b1602e337f8ceb7c5a8483949d5
Tommy <thomaskclark@gmail.com>
2016-12-09 11:02:33 -0800

    Shorten variable names to condense code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,28 +11,34 @@ emp_age = gets.to_i
 puts "What year were you born?"
 birth_year = gets.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order you some? (Y/N)"
-garlic_bread = gets.chomp.upcase
-until (garlic_bread == "Y" or garlic_bread == "N")
-	puts "Please enter 'Y' or 'N'." 
-	garlic_bread = gets.chomp.upcase
+real_year = Time.now.year - emp_age
+if real_year == birth_year
+  real_year = true
+else real_year = false
 end
 
-if garlic_bread == "Y"
-	garlic_bread = true
-else garlic_bread = false
+puts "Our company cafeteria serves garlic bread. Should we order you some? (Y/N)"
+garlic = gets.chomp.upcase
+until (garlic == "Y" || garlic == "N")
+	puts "Please enter 'Y' or 'N'." 
+	garlic = gets.chomp.upcase
+end
+
+if garlic == "Y"
+	garlic = true
+else garlic = false
 end
 
 puts "Would you like to enroll in the company's health insurance? (Y/N)"
-health_insurance = gets.chomp.upcase
-until (health_insurance == "Y" or health_insurance == "N")
+insurance = gets.chomp.upcase
+until (insurance == "Y" || insurance == "N")
 	puts "Please enter 'Y' or 'N'." 
-	health_insurance = gets.chomp.upcase
+	insurance = gets.chomp.upcase
 end
 
-if health_insurance == "Y"
-	health_insurance = true
-else health_insurance = false
+if insurance == "Y"
+	insurance = true
+else insurance = false
 end
 
 puts "Please list ALL allergies, one at a time. Enter 'done' once you are finished."
@@ -46,9 +52,9 @@ until done
 end
 
 verdict = nil
-verdict = "Probably not a vampire." if birth_year == Time.now.year - emp_age && (garlic_bread || health_insurance)
-verdict = "Probably a vampire." if birth_year != Time.now.year - emp_age && (!garlic_bread || !health_insurance) || allergy == "sunshine"
-verdict = "Almost certainly a vampire." if birth_year != Time.now.year - emp_age && !garlic_bread && !health_insurance
+verdict = "Probably not a vampire." if real_year && (garlic || insurance)
+verdict = "Probably a vampire." if !real_year && (!garlic || !insurance) || allergy == "sunshine"
+verdict = "Almost certainly a vampire." if !real_year && !garlic && !insurance
 verdict = "Definitely a vampire." if emp_name.downcase == "drake cula" || emp_name.downcase == "tu fang"
 verdict = "Results inconclusive." if verdict == nil
 
