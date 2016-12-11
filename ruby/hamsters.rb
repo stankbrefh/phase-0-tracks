706f4bf14ada6773d615d89c564259692b5b4232
Tommy <thomaskclark@gmail.com>
2016-12-10 16:27:11 -0800

    Update semantics and syntax

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,32 +2,33 @@ puts "What is the hamster's name?"
 name = gets.chomp
 
 puts "Volume level? (1-10)"
-volume = gets.to_i
+vol = gets.to_i
 
 puts "Fur color?"
-fur = gets.chomp
+color = gets.chomp
 
 puts "Is the hamster a good candidate for adoption? (y/n)"
-good_candidate = gets.chomp.downcase
-if good_candidate == "y"
-  good_candidate = "lovable"
-elsif good_candidate == "n"
-  good_candidate = "a work in progress"
-else
-  good_candidate = "'status: unknown'"
+good = gets.chomp.downcase
+if good == "y"
+	good = "a lovable lump"
+elsif good == "n"
+	good = "a work in progress"
+else good = "of 'status: unknown'"
 end
-
 
 puts "What is the hamster's estimated age?"
 age = gets.chomp
 if age.empty?
-  age = nil
-else
-  age = age.to_i
+	age = nil
+else age = age.to_i
 end
 
 puts "The hamster's name is #{name}."
-puts "The hamster's volume level is #{volume}."
-puts "The hamster has #{fur} fur."
-puts "You said this hamster is #{good_candidate}."
-puts "This hamster is #{age}."
+puts "The hamster's volume level is #{vol}."
+puts "It has #{color} fur."
+puts "You said this hamster is #{good}."
+
+if age == nil
+	puts "Nobody knows the age of this sad sack."
+else puts "This happy hamster is #{age} years old."
+end
