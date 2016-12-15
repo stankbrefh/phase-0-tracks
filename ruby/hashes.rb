8603ee08192fe6a7da8d00d8be90695478215eaa
Tommy <thomaskclark@gmail.com>
2016-12-14 16:37:18 -0800

    Allow user to update key value

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -8,19 +8,38 @@
 
 client_data = {}
 
-puts "Hey there! Lemme get your name:"
+puts "Hey there! Lemme get their name:"
 client_data[:name] = gets.chomp
 
-puts "You look #{rand(20...60)}, but what's your real age?"
+puts "They look #{rand(20...60)}, but what's their real age?"
 client_data[:age] = gets.to_i
 
 puts "How many brats?"
 client_data[:brats] = gets.to_i
 
-puts "What kind of theme you want?"
+puts "What kind of theme they want?"
 client_data[:theme] = gets.chomp
 
-puts "Have any pets (y/n)?"
+puts "Have any pets? (y/n)"
 client_data[:pets] = gets.chomp.downcase == "y" ? true : false
 
-puts client_data.values
+puts "Let me get this straight:"
+client_data.each {|key, value| puts "#{key}: #{value}"}
+
+puts "Did you mess up? (y/n)"
+mistake = gets.chomp.downcase == "y" ? true : false
+
+if mistake == true
+  puts "Gah! Of course you did. Now what do you need to change? (name, age, brats, pets, or theme)"
+  update = gets.chomp.to_sym
+  puts "Enter the correct info:"
+  new_value = gets.chomp
+  client_data[update] = new_value
+  puts "Finally!"
+  client_data.each {|key, value| puts "#{key}: #{value}"}
+  puts "Their fate is sealed!"
+else
+  puts "Muahahaha!"
+end
+
+puts "Now all their monies are belong to us!!!"
