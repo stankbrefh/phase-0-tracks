74a4a89ed2d80b10e9c114beb54d09cd04473a6f
Tommy <thomaskclark@gmail.com>
2016-12-15 13:37:00 -0800

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,103 @@
+# Nested data structure
+hearthstone_classes = {
+  druid: {
+    hero: 'Malfurion',
+    power: 'Shapeshift',
+    legends: [
+      'Cenarius',
+      'Malorne',
+      'Aviana',
+      'Fandral Staghelm',
+      'Kun the Forgotten King'
+    ]
+  },
+  hunter: {
+    hero: 'Rexxar',
+    power: 'Steady Shot',
+    legends: [
+      'King Krush',
+      'Acidmaw',
+      'Dreadscale',
+      'Gahz\'rilla',
+      'Princess Huhuran',
+      'Knuckles'
+    ]
+  },
+  mage: {
+    hero: 'Jana',
+    power: 'Fireblast',
+    legends: [
+      'Archmage Antonidas',
+      'Flame Leviathan',
+      'Rhonin',
+      'Anomalus',
+      'Inkmaster Solia'
+    ]
+  },
+  paladin: {
+  	hero: 'Luther',
+  	power: 'Reinforce',
+  	legends: [
+      'Tirion Fordring',
+      'Bolvar Fordragon',
+      'Eadric the Pure',
+      'Ragnaros, Lightlord',
+      'Wickerflame Burnbristle'
+    ]
+  },
+  priest: {
+    hero: 'Anduin',
+    power: 'Lesser Heal',
+    legends: [
+      'Prophet Velen',
+      'Vol\'jin',
+      'Confessor Paletress',
+      'Herald Volazj',
+      'Raza the Chained'
+    ]
+  },
+  rogue: {
+    hero: 'Valeera',
+    power: 'Dagger Mastery',
+    legends: [
+      'Edwin VanCleef',
+      'Trade Prince Gallywix',
+      'Anub\'arak',
+      'Xaril, Poisoned Mind',
+      'Shaku, the Collector'
+    ]
+  },
+  shaman: {
+    hero: 'Thrall',
+    power: 'Totemic Call',
+    legends: [
+      'Al\'Akir the Windlord',
+      'Neptulon',
+      'The Mistcaller',
+      'Hallazeal the Ascended',
+      'White Eyes'
+    ]
+  },
+  warlock: {
+    hero: 'Guldan',
+    power: 'Life Tap',
+    legends: [
+      'Lord Jaraxxus',
+      'Mal\'Ganis',
+      'Wilfred Fizzlebang',
+      'Cho\'gall',
+      'Krul the Unshackled'
+    ]
+  },
+  warrior: {
+    hero: 'Garrosh Hellscream',
+    power: 'Armor Up!',
+    legends: [
+      'Grommash Hellscream',
+      'Iron Juggernaut',
+      'Varian Wrynn',
+      'Malkorok',
+      'Hobart Grapplehammer'
+    ]
+  }
+}
