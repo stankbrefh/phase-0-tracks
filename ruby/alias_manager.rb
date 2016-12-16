9e7fe1518955c338d7346a2cb0a7e97b2affcea6
Tommy <thomaskclark@gmail.com>
2016-12-16 14:59:05 -0800

    Update method to translate vowels and consonants

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,21 @@
-# Swapping the first and last name.
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou',
-# and all of the consonants (everything else besides the vowels) to the next 
-# consonant in the alphabet. 
-# So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+# Use split to create an array containing the element(s) of the user's input. 
+# Invoke block once for each element in that array using map. 
+# The block calls the translate method on the element and stores the new values in the array.
+# The order of the elements in that array is reversed and then the elements are joined together.
 
-# Swap first and last name
-def name_swap(name)
-  name.split(' ').reverse.join(' ')
+# Get input from user
+puts 'name?'
+name = gets.chomp
+
+# Method to change user's name
+def aliaser(name)
+  consonants = 'bcdfghjklmnpqrstvwxyz'
+  key = 'cdfghjklmnpqrstvwxyzb'
+
+  # Replace (translate) characters from the first argument with characters (of the same index) from the second argument 
+  name = name.downcase.split.map! {|name| name.tr('aeiou', 'eioua').tr(consonants, key).capitalize}
+  name.reverse.join(' ')
 end
+
+# Display new alias to the user.
+puts aliaser(name)
