b3b321b0f60b8f800afc5f60c74a60aaa906b9b7
Tommy <thomaskclark@gmail.com>
2016-12-20 10:21:25 -0800

    Define class and initialize instance of that class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -10,3 +10,26 @@
 # Jump
 # Scamper
 # Woof
+
+# Define class Puppy
+class Puppy
+  def initialize(breed, name)
+    # Instance variables
+    @breed = breed
+    @name = name
+  end
+
+  def bark
+    puts 'Ruff! Ruff!'
+  end
+
+  def display
+    puts "I am of #{@breed} breed and my name is #{@name}"
+  end
+end
+
+# Make an object
+d = Puppy.new('Doge', 'Puppers')
+
+d.bark
+d.display
