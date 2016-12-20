5bbe883776b25587dce438e08ace2b44c97214d8
Tommy <thomaskclark@gmail.com>
2016-12-20 10:16:44 -0800

    Add characteristics for Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# SPECIES ------------------------
+# Puppers
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# Scamper
+# Woof
