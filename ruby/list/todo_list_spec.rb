ddda1e929485e7089e8dc91e787235602ce063c0
Tommy <thomaskclark@gmail.com>
2016-12-22 22:43:21 -0800

    Add method to pass delete_item RSpec test

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -12,10 +12,10 @@ describe TodoList do
     expect(list.get_items).to eq ["do the dishes", "mow the lawn", "mop"]
   end
 
-  # it "deletes an item" do
-  #   list.delete_item("do the dishes")
-  #   expect(list.get_items).to eq ["mow the lawn"]
-  # end
+  it "deletes an item" do
+    list.delete_item("do the dishes")
+    expect(list.get_items).to eq ["mow the lawn"]
+  end
 
   # it "retrieves an item by index" do
   #   expect(list.get_item(0)).to eq "do the dishes"
