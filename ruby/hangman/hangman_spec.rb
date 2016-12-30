6c3723c10c686a31c92ae3e2f96398eb09b51799
Tommy <thomaskclark@gmail.com>
2016-12-29 19:18:48 -0800

    Add RSpec tests for user input limitations

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,10 +1,17 @@
 require_relative 'hangman'
 
 describe Game do
-  let(:game) { Game.new }
+  let(:game) { Game.new('test') }
 
-  it "" do
-    expect(game.).to eq 
+  it "can check if input letter is found in word" do
+    expect(game.guess('t')).to eq true
+    expect(game.guess('q')).to eq false
   end
-
+  
+  it "limits input to a single letter (A-Z)" do
+  	expect(game.guess('abc')).to eq false
+  	expect(game.guess('tes')).to eq false
+  	expect(game.guess('3')).to eq false
+  end
+  
 end
