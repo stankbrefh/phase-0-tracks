c6d2a0e75ab83da9f4dcd21cd8839571d8ee8d64
Tommy <thomaskclark@gmail.com>
2016-12-29 19:03:07 -0800

    Add methods to limit user input to letters

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -3,14 +3,15 @@ class Game
   attr_accessor :word
 
   def initialize(word)
-  	@word = word
-  	@guesses = word.length
-  	@current_state = Array.new(word.length) { |i| "_" }
+    @word = word
+    @guesses = word.length
+    @current_state = Array.new(word.length) { |i| "_" }
     @wrong_letters = []
     @gameover = false
   end
   
   def guess(letter)
+    correct = false
     if @wrong_letters.include? letter
       puts 'How\'s about you try a different letter, wise guy!'
     else
@@ -19,16 +20,38 @@ class Game
           @word.length.times do |i|
             if @word[i] == letter
               @current_state[i] = letter
+              correct = true
             end
           end
         end
         @guesses -= 1
     end
+    return correct
   end
 
-  def gameover?
+  def gameover
     @gameover = (@current_state.join == @word)
   end
+
+end
+
+def is_letter(letter)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  if not letter.empty?
+    return (alphabet.include? letter[0]) && (letter.length == 1)
+  else
+    return false
+  end
+end
+
+def is_word(word)
+  word = word.downcase
+  word.length.times do |i|
+    if not is_letter(word[i])
+      return false
+    end
+  end
+  return true
 end
 
 hanged = <<HANG
@@ -41,22 +64,35 @@ hanged = <<HANG
 HANG
 
 puts 'Enter word:'
-input = gets.chomp
+input = gets.chomp.downcase
+
+until is_word(input)
+  puts 'Please only use letters A-Z in your word.'
+  input = gets.chomp.downcase
+end
+
 game = Game.new(input)
 
-until game.guesses == 0 || game.gameover?
+until game.guesses == 0 || game.gameover
   puts 'Attempts remaining: ' + game.guesses.to_s
   puts 'Enter letter:'
-  current_guess = gets.chomp
+  current_guess = gets.chomp.downcase
+
+  until is_letter(current_guess)
+    puts 'Please ONLY guess with a single letter: A-Z'
+    current_guess = gets.chomp.downcase
+  end
+
   game.guess(current_guess)
-  puts 'Status: ' + game.current_state.join(' ')
-  if game.gameover?
+  puts 'Status: ' + game.current_state.join(' ').dump
+
+  if game.gameover
     puts 'GGWP'
     break
   end
 end
 
-if not game.gameover?
+if not game.gameover
   puts 'You\'re a loser and nobody likes you!'
   puts hanged
 end
---
+++ ruby/hangman/hangman_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'hangman'
+
+describe Game do
+  let(:game) { Game.new }
+
+  it "" do
+    expect(game.).to eq 
+  end
+
+end
