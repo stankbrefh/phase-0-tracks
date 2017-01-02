c0af740215660d84d90e62a03357ad6f73679978
Tommy <thomaskclark@gmail.com>
2017-01-02 12:04:11 -0800

    Add RSpec test for Driver class

--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -12,12 +12,30 @@ describe Game do
     expect(game.hint).to eq ["_","_","s","_"]
   end
 
-  it "checks to see if the game is over or not" do
-    expect(game.gameover).to eq false
+  it "checks to see if the game is won or not" do
+    expect(game.game_won).to eq false
     game.guess('t')
     game.guess('e')
     game.guess('s')
-    expect(game.gameover).to eq true
+    expect(game.game_won).to eq true
+  end
+
+  it "decrements the number of guesses remaining after each guess" do
+    game.guess('q')
+    expect(game.guesses).to eq 6
+    game.guess('e')
+    expect(game.guesses).to eq 5
+  end
+
+end
+
+describe Driver do
+  let(:driver) { Driver.new }
+
+  it "determines if input is single letter, A-Z" do
+    expect(driver.is_letter('a')).to eq true
+    expect(driver.is_letter(' ')).to eq false
+    expect(driver.is_letter('abc')).to eq false
   end
 
 end
