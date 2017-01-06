e34ee0201f7c4b26e799df832cd4628ea9527506
Tommy <thomaskclark@gmail.com>
2017-01-05 16:11:13 -0800

    Add pseudocode for function that reverses a string

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// PSEUDOCODE
+
+// Create a function that takes a string as a parameter.
+// Declare a result variable and set it equal to an empty string.
+// For each letter, add the current letter to the result.
+// Return the result.
+// Log the the result to the console. 
