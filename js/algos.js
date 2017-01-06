8fa49940a5257950fe549800d0830ed04087f00a
Tommy <thomaskclark@gmail.com>
2017-01-05 19:11:08 -0800

    Add pseudocode to find longest phrase

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// RELEASE 0: FIND THE LONGEST PHRASE
+// PSEUDOCODE
+// Create a function that takes an array of strings as a parameter.
+// Set a variable for an empty string.
+// Iterate through the array and compare the length of the string to the current longest.
+// Replace the longest string if the current string is longer. 
+// Return the longest string after all iterations are complete.
