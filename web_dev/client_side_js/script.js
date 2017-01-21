38e38581d4ad46cf839b278d4f57cc00b42a0a65
Tommy <thomaskclark@gmail.com>
2017-01-20 18:40:20 -0800

    Add color to animation and call functions

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,9 @@
+var blue = [196, 77, 55];
+var word = "JavaScript";
+
+drawName(word, blue);
+bounceBubbles();
+
 document.getElementById("myBtn").addEventListener("click", function(){
     document.getElementById("demo").innerHTML = "Hello World";
 });
